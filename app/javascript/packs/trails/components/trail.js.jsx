06db203461e20687d53f5170955e610d5df63089
Brendan Thomas <bt331609@gmail.com>
2017-04-17 14:07:22 -0400

    some styles

--- app/javascript/packs/trails/components/trail.js.jsx
+++ app/javascript/packs/trails/components/trail.js.jsx
@@ -11,6 +11,7 @@ class Trail extends React.Component {
         <h3>{this.props.trail.name}</h3>
         <span className='distance'>{this.props.trail.distance} mi</span>
         <i className='fa fa-check'></i>
+        <a className='map-link' href="#"><i className='fa fa-map'></i></a>
       </div>
     )
   }
