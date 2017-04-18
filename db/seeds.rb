bdd6f069e84c8dba76e5823854b832a32cc3d942
Brendan Thomas <bt331609@gmail.com>
2017-04-18 16:33:03 -0400

    add devise and user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 Trail.create(name: 'Ledges', distance: 2.2, trail_type: 'Loop')
 Trail.create(name: 'Old Carriage Trail', distance: 5.25, trail_type: 'Loop')
+
+User.create(email: 'randomdude@example.com', password: 'secret')
