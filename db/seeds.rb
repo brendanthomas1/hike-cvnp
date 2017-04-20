62212c2995a6a923c98089f7e88fa160cf52f120
Brendan Thomas <bt331609@gmail.com>
2017-04-20 16:36:34 -0400

    add admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,4 @@ Trail.create(name: 'Ledges', distance: 2.2, trail_type: 'Loop')
 Trail.create(name: 'Old Carriage Trail', distance: 5.25, trail_type: 'Loop')
 
 User.create(email: 'randomdude@example.com', password: 'secret')
+User.create(email: 'operator@test.com', password: 'testing!', admin: true)
